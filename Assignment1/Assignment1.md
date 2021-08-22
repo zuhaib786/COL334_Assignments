5915c884faee11d7557aaa6fca2b6738a29d598c
zuhaib786 <suhaibulsaman@gmail.com>
2021-08-22 09:42:40 +0530

    submission

--- Assignment1/Assignment1.md
+++ Assignment1/Assignment1.md
@@ -6,7 +6,6 @@ The ip address changes to -- on connecting to my mobile hotspot
 The command nslookup www.google.com shows the ip address as 142.251.42.68<br>
 The command nslookup www.facebook.com shows the ip address as 157.240.198.35<br>
 ## part c
-Ping command TTL is min(TTL supported by the server, TTL input)
 On www.google.com, I can send packets of maximum size 1472 bytes after which request times out.<br>
 On www.iitd.ac.in, I can send packets of maximum size 1472 bytes after which request times out.<br>
 On www.facebook.com, I can send packets of maximum size 1472 bytes after which request times out.<br>
---
+++ Assignment1/traceroute.py
@@ -0,0 +1,73 @@
+import subprocess
+import re
+import time
+import matplotlib
+import matplotlib.pyplot as plt
+class Traceroute:
+    def __init__(self, destination, count):
+        self.destination = destination
+        self.version = "-4"
+        self.data =[]
+        self.RTT = []
+        self.count = count
+    def run(self):
+        for ttl in range(1, 31):
+            bashCmd = ["ping", self.version, "-i", str(ttl), self.destination]
+            process = subprocess.Popen(bashCmd, stdout = subprocess.PIPE)
+            output, error =  process.communicate()
+            process.kill()
+            output = output.split(b"\n")
+            output = [i.decode('utf-8') for i in output]
+            print("TTL = ", ttl)
+            s = None
+            for i in range(2, 5):
+                if (output[i].find('expired') == -1 and output[i].find('timed out') == -1):
+                    print(output[i])
+                    print(output[i].split()[4].split("=")[1][:-2])
+                    self.RTT.append(int(output[i].split()[4].split("=")[1][:-2]))
+                    self.data.append(ttl)
+                    plt.plot(self.data, self.RTT)
+                    plt.xlabel("Number of Hops")
+                    plt.ylabel("Round Trip Time")
+                    plt.savefig("TTL vs RTT.png")
+                    plt.show()
+                    return
+                elif (output[i].find('timed out') == -1):
+                    s = output[i]
+            if(s==None):
+                self.RTT.append(0.0)
+                self.data.append(ttl)
+                print("*   *    *  RTTT = 0 due to timeout")
+                
+            else:
+                s = s.split()
+                bashCmd = ["ping", self.version, s[2][:-1]]
+                print(s[2][:-1])
+                process = subprocess.Popen(bashCmd, stdout = subprocess.PIPE)
+                output, error =  process.communicate()
+                process.kill()
+                output = output.split(b"\n")
+                output = [i.decode('utf-8') for i in output]
+                cnt = 0
+                time = 0
+                for i in range(2, 5):
+                    if(output[i].find('expired')==-1 and output[i].find('timed out') == -1):
+                        st = output[i].split()[4].split("=")[1][:-2]
+                        print(st, "ms",end = ' ')
+                        time+=int(st)
+                        cnt+=1
+                    else:
+                        print("*",end = ' ')
+                if(cnt == 0):
+                    self.data.append(ttl)
+                    self.RTT.append(0)
+                    print("RTT = 0 due to ping failure")
+                else:
+                    self.data.append(ttl)
+                    self.RTT.append(time/cnt)
+                    print("RTT = ", time/cnt)
+print("Enter the destination to Traceroute: ")
+url = input().split()[0]
+print("Tracing route to ", url)
+tracker = Traceroute(url, 0)
+tracker.run()
